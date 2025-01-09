aaa459b7af993f95df68ac1afaa0f720d86be790
Nicolas Garric <nicolasdfgarric@gmail.com>
2025-01-09 17:19:51 +0100

    Cleaning code and looking for new contact form

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+class ContactMailer < ApplicationMailer
+  def send_request
+    @contact_request = params[:contact_request]
+    mail(to: "nicolasdfgarric@gmail.com", subject: "Nouvelle demande de devis")
+  end
 end
