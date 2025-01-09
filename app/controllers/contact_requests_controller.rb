c3a903a8fb35ecc77baa91ac6ff87c3dc8433b65
Nicolas Garric <nicolasdfgarric@gmail.com>
2025-01-09 17:09:23 +0100

    Contact request model and controller created with specific view

--- app/controllers/contact_requests_controller.rb
+++ app/controllers/contact_requests_controller.rb
@@ -1,2 +1,25 @@
 class ContactRequestsController < ApplicationController
+  def new
+    @contact_request = ContactRequest.new
+  end
+
+  def create
+    @contact_request = ContactRequest.new(contact_request_params)
+
+    if @contact_request.valid?
+      # Exemple d'action à effectuer, comme envoyer un email
+      ContactMailer.with(contact_request: @contact_request).send_request.deliver_now
+      flash[:notice] = "Votre demande a été envoyée avec succès."
+      redirect_to new_contact_request_path
+    else
+      flash[:alert] = "Veuillez corriger les erreurs du formulaire."
+      render :new
+    end
+  end
+
+  private
+
+  def contact_request_params
+    params.require(:contact_request).permit(:first_name, :last_name, :company_name, :email, :phone_number, :message)
+  end
 end
